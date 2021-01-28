4335d1d3f20b119357781710d2f24588a81ad7e9
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-01-28 19:18:57 +0100

    add Accessor for views and buffers
    * add read-only access
    * add readAccess()/AccessMode
    * add access mode tags for read, write and read-write access
    * only allow writing via WriteAccess accessors
    * rewrite tests and examples
    * add tests
    * add example for an accessor with a projection
    * implement constraining Accessors
    * add some documentation
    * declare variable templates inline when supported
    * add BuildAccessor customization point
    * view accessors are separate from generic Accessor functionality
    * add MemoryHandle trait to query memory handle of an accessor
    * add BufferAccessor convenience alias
    * mark kernels without executed code noexcept to fix a warning

--- include/alpaka/alpaka.hpp
+++ include/alpaka/alpaka.hpp
@@ -123,7 +123,9 @@
 #include <alpaka/mem/buf/BufOmp5.hpp>
 #include <alpaka/mem/buf/BufUniformCudaHipRt.hpp>
 #include <alpaka/mem/buf/Traits.hpp>
+#include <alpaka/mem/view/Accessor.hpp>
 #include <alpaka/mem/view/Traits.hpp>
+#include <alpaka/mem/view/ViewAccessor.hpp>
 #include <alpaka/mem/view/ViewCompileTimeArray.hpp>
 #include <alpaka/mem/view/ViewPlainPtr.hpp>
 #include <alpaka/mem/view/ViewStdArray.hpp>
--- test/unit/math/src/sincos.cpp
+++ test/unit/math/src/sincos.cpp
@@ -36,8 +36,11 @@ class SinCosTestKernel
 {
 public:
     ALPAKA_NO_HOST_ACC_WARNING
-    template<typename TAcc, typename FP>
-    ALPAKA_FN_ACC auto operator()(TAcc const& acc, bool* success, FP const arg) const -> void
+    template<typename TAcc, typename TMemoryHandle, typename FP>
+    ALPAKA_FN_ACC auto operator()(
+        TAcc const& acc,
+        alpaka::Accessor<TMemoryHandle, bool, alpaka::Idx<TAcc>, 1, alpaka::WriteAccess> const success,
+        FP const arg) const -> void
     {
         // if arg is hardcoded then compiler can optimize it out
         // (PTX kernel (float) was just empty)
@@ -47,7 +50,7 @@ public:
         FP result_cos = 0.;
         alpaka::math::sincos(acc, arg, result_sin, result_cos);
         ALPAKA_CHECK(
-            *success,
+            success[0],
             almost_equal(acc, result_sin, check_sin, 1) && almost_equal(acc, result_cos, check_cos, 1));
     }
 };
